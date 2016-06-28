d5c4a48151b5cfb31acd233481ff740af1c9d78e
Sandrino Di Mattia <contact@sandrino.be>
2016-06-28 16:29:52 +0200

    Additional logging when validating the uninstall hook

--- server/lib/middlewares/validateHookToken.js
+++ server/lib/middlewares/validateHookToken.js
@@ -8,6 +8,8 @@ module.exports = (hookPath) =>
   (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
       const token = req.headers.authorization.split(' ')[1];
+      logger.debug('Extension Hook validation token:', token);
+
       const isValid = jwt.verify(token, config('EXTENSION_SECRET'), {
         audience: path.join(config('WT_URL'), hookPath),
         issuer: `https://${config('AUTH0_DOMAIN')}`
