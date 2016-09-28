90a792b781e6a21a667f98fc6d856739b3fdac2d
roman.bazarov <trexbazarov@gmail.com>
2016-09-28 20:00:22 +0300

    fixes

--- client/components/RulesTable.jsx
+++ client/components/RulesTable.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import { Table, TableAction, TableCell, TableBody, TableIconCell, TableTextCell, TableHeader, TableColumn, TableRow } from './Dashboard';
+import {
+  Table,
+  TableAction,
+  TableCell,
+  TableBody,
+  TableIconCell,
+  TableTextCell,
+  TableHeader,
+  TableColumn,
+  TableRow
+} from './Dashboard';
 import Alert from 'react-s-alert';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/slide.css';
@@ -16,48 +26,50 @@ export default class RulesTable extends Component {
   }
 
   onChangeManual = (e) => {
-      let elements = document.getElementsByClassName("isManualRule");
-      let manualRules = [];
-      for(var i=0,length=elements.length;i<length;i++) {
-          if(elements[i].checked) {
-              manualRules.push(elements[i].value);
-          }
+    const rules = this.props.rules;
+    let manualRules = [];
+    rules.map((isManual, index) => {
+      if (this.refs[index].checked) {
+        manualRules.push(this.refs[index].value);
       }
-      this.props.saveManualRules({names:manualRules}).then(() => {
-          Alert.info('Manual rules were updated.',{
-              effect: 'slide',
-              limit: 1
-          });
+    });
+    if (manualRules.length > 0)
+      this.props.saveManualRules({ names: manualRules }).then(() => {
+        Alert.info('Manual rules were updated.', {
+          effect: 'slide',
+          limit: 1
+        });
       });
   }
 
   render() {
     const { rules } = this.props;
     return (
-     <div>
-         <Alert stack={{limit: 3}} position='top' />
-      <Table>
-        <TableHeader>
-          <TableColumn width="80%">Name</TableColumn>
-          <TableColumn width="20%">Manual Rule</TableColumn>
-        </TableHeader>
-        <TableBody>
-        {rules.map((isManual, index) => {
-          return (
-              <TableRow key={index}>
+      <div>
+        <Alert stack={{ limit: 3 }} position='top' />
+        <Table>
+          <TableHeader>
+            <TableColumn width="80%">Name</TableColumn>
+            <TableColumn width="20%">Manual Rule</TableColumn>
+          </TableHeader>
+          <TableBody>
+            {rules.map((isManual, index) => {
+              return (
+                <TableRow key={index}>
                   <TableTextCell>{index}</TableTextCell>
                   <TableTextCell>
-                      <div className="switch-animate">
-                          <input className="uiswitch isManualRule" value={index} defaultChecked={isManual} type="checkbox" name="iSManualRule" />
-                      </div>
+                    <div className="switch-animate">
+                      <input className="uiswitch isManualRule" value={index} defaultChecked={isManual} type="checkbox"
+                             name="iSManualRule" ref={index} />
+                    </div>
                   </TableTextCell>
-              </TableRow>
-            );
-        })}
-        </TableBody>
-      </Table>
-         <button className="btn btn-success pull-right" onClick={this.onChangeManual.bind(this)}>Update Manual Rules</button>
-     </div>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+        <button className="btn btn-success pull-right" onClick={this.onChangeManual}>Update Manual Rules</button>
+      </div>
     );
   }
 }
