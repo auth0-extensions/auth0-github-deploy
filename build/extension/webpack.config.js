6c75e7398c57768fd604e886187816195c9123e5
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-09-30 12:18:32 +0300

    bundle size bug fixed

--- build/extension/webpack.config.js
+++ build/extension/webpack.config.js
@@ -40,6 +40,7 @@ module.exports = externalModules.then((externals) => {
 
   // Additional dependencies that are available in webtask.
   externals.compatible['auth0'] = 'auth0@2.4.0';
+  externals.compatible['auth0-oauth2-express'] = 'auth0-oauth2-express@1.1.5';
   externals.compatible['nconf'] = true;
   externals.compatible['node-uuid'] = true;
   externals.compatible['jade'] = true;
